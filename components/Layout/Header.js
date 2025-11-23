b48f97e875766aa6fd18ccde6746f0af0ef518d7
Kyle Ma <49329975+Hanamizu2002@users.noreply.github.com>
2025-11-23 15:49:14 +0800

    Update logo image source in Header component

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
             >
                 <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
                     <div className="col-start-1 col-end-2 flex items-center">
-                        <img src="https://ns.alsace.team/service/images/logo/logo-web.webp" alt="Logo"
+                        <img src="https://ns.alsace.team/service/images/logo/25/AlsaceIndustrialPark.svg" alt="Logo"
                              className={"h-12 w-auto"}/>
                     </div>
                     <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
